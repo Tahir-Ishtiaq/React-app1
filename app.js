6217a9890b0ccc1a60d495cbf3c9602efd13991c
Tahir-Ishtiaq <tahir.ishtiaq3203@gmail.com>
2024-11-29 14:08:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/* typing animation*/
+
+let typed=new Typed('.typing', {
+    strings: ['Web Developer', 'Graphic Designer', 'Content Writer','Web Designer'],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true
+})
